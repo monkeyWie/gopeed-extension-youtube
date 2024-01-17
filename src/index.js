e48ad60aa133dfd5e5649e459e28f911b40e942e
liwei <liwei-8466@qq.com>
2024-01-17 10:18:33 +0800

    feat: support youtu.be host

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import ytdl from 'ytdl-core';
 
 // https://www.youtube.com/watch?v=aqz-KE-bpKQ
 gopeed.events.onResolve(async (ctx) => {
-  const video = await ytdl.getInfo(ctx.req.url);
+  var url = ctx.req.url;
+  var ytbUrl = url;
+  if (url.includes('youtu.be/')) {
+    ytbUrl = new URL(url).pathname.substring(1);
+  }
+  const video = await ytdl.getInfo(ytbUrl);
   const formats = ytdl.filterFormats(video.formats, 'videoandaudio');
   formats.sort((a, b) => b.bitrate - a.bitrate);
   const bestFormat = gopeed.settings.quality === 'lowest' ? formats[formats.length - 1] : formats[0];
-  gopeed.logger.debug(JSON.stringify(bestFormat));
   let fmt = '.mp4';
   if (bestFormat.mimeType) {
     fmt = '.' + bestFormat.mimeType.split(';')[0].split('/')[1];
